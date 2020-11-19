f1783666ee56223d418da3bbb8c31e01e37cb974
alexabotello <abotello20@yahoo.com>
2020-11-18 22:40:10 -0600

    commit tree 0aafc705641ebd985e919a3c84b7c5a1ef35553b

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    puts params
+    @input = PigLatinizer.new(params["user_phrase"])
+    erb :piglatinize
+  end
+
+  get '/piglatinize' do
+    erb :piglatinize
+  end
+
+  post '/piglatinize' do
+    puts params
+    @input = PigLatinizer.new
+    erb :piglatinize
+  end
 end
